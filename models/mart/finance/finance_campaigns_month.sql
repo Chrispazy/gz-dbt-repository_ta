f29344a736cc47bdc9948c16c09d12c50c2f85d9
CHRISTOPHE DE LA PAZ Y FELIZ <delapazc4@gmail.com>
2025-02-12 09:39:08 +0000

    update table finance campaigns month

--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -4,8 +4,8 @@ SELECT
      ROUND(SUM(average_basket),2) AS average_basket,
      SUM(operational_margin) AS operational_margin,
      SUM(ads_cost) AS ads_cost,
-     SUM(ads_impression) AS ads_impression,
-     SUM(ads_clicks) AS ads_clicks,
+     SUM(impression) AS ads_impression,
+     SUM(click) AS ads_clicks,
      SUM(quantity) AS quantity,
      SUM(revenue) AS revenue,
      SUM(purchase_cost) AS purchase_cost,
@@ -13,9 +13,9 @@ SELECT
      SUM(shipping_fee) AS shipping_fee,
      SUM(logcost) AS logcost,
      SUM(ship_cost) AS ship_cost,
- FROM {{ ref('int_campaigns_day') }}
- FULL OUTER JOIN {{ ref('finance_days') }}
-     USING (date_date)
- GROUP BY datemonth
- ORDER BY datemonth desc
+FROM {{ ref('int_campaigns_day') }}
+FULL OUTER JOIN {{ ref('finance_days') }}
+USING (date_date)
+GROUP BY datemonth
+ORDER BY datemonth desc
 
