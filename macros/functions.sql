5e9a347c5cc10cf1066bd480e9e34640fe5f7413
CHRISTOPHE DE LA PAZ Y FELIZ <delapazc4@gmail.com>
2025-02-12 12:14:28 +0000

    ajout des fonctions

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,3 +1,3 @@
- {% macro margin_percent(revenue, purchase_cost, precision=2) %}
-     ROUND( SAFE_DIVIDE( ({{ revenue }} - {{ purchase_cost }}) , {{ revenue }} ) , {{ precision }})
- {% endmacro %}
+{% macro margin_percent(revenue,purchase_price,quantity) %}
+ROUND(SAFE_DIVIDE(({{revenue}}-({{purchase_price}}*{{quantity}})),{{revenue}})*100,2) AS margin_percent
+{% endmacro %}
